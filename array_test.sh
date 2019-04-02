3b90d73eb60e1fc85c522c9437228c06cdb2146b
LLSUB <leegi5114@gmail.com>
2019-04-02 14:34:42 +0900

    my first update

--- array_test.sh
+++ array_test.sh
@@ -1,1 +1,16 @@
-abc
+#!/bin/bash
+
+declare -a worlds
+
+while [ 1 ]
+do
+	read -p "Enter a new world: " input
+	if [ $input = "list" ]; then
+		echo ${worlds[*]}
+	elif [ $input = "quit" ]; then
+		exit 0
+	else
+		worlds+=($input)
+		echo "Length: "${#worlds[@]}
+	fi
+done
